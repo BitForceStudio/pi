6e34c61752ded8ffaf861e0a47285a46f31a31b1
BitForceStudio <bitforce.studio@gmail.com>
2016-06-18 17:17:41 +0100

    update the code
    update the code

--- vrcar/vrcar.py
+++ vrcar/vrcar.py
@@ -5,26 +5,31 @@ motor2 = Motor(6, 22)
 motor2_enable = OutputDevice(17, initial_value=1)
 motor3 = Motor(23, 16)
 motor3_enable = OutputDevice(12, initial_value=1)
-i=0
-while i<10:
-    motor2.forward() # full speed forwards
-    motor3.forward() # full speed forwards
 
-    sleep(3)
-
-    motor2.backward() # full speed backwards
-    motor3.backward() # full speed backwards
-
-    sleep(3)
-
-    motor2.forward() # stop the motor
-    motor3.stop() # stop the motor
-
-    sleep(1) 
-
+def left_turn():
     motor3.forward()
-    motor2.stop()
+    motor2.backward()
+    sleep(2)
 
-    sleep(1)
+def right_turn():
+    motor3.backward()
+    motor2.forward()
+    sleep(2)
 
-    i=i+1
+if __name__ == '__main__':
+    cmd=''
+    while cmd is not 'exit':
+        if cmd=='w':
+            motor2.forward() # full speed forwards
+            motor3.forward() # full speed forwards
+            sleep(2)
+        elif cmd=='s':
+            motor2.backward() # full speed backwards
+            motor3.backward() # full speed backwards
+            sleep(2)
+        elif cmd=='a':
+            left_turn()
+        elif cmd=='d':
+            right_turn()
+        elif cmd=='e':
+            break
