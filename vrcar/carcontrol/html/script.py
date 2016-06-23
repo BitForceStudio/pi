d370bc33dacf401eae79c7751db1dd9120d9213b
BitForceStudio <bitforce.studio@gmail.com>
2016-06-23 23:46:11 +0100

    try
    try to understand how it works

--- vrcar/carcontrol/html/script.py
+++ vrcar/carcontrol/html/script.py
@@ -1,24 +1,44 @@
+from gpiozero import Motor, OutputDevice
+from time     import sleep
+
 import webiopi
 import datetime
 
+motor2 = Motor(6, 22)
+motor2_enable = OutputDevice(17, initial_value=1)
+motor3 = Motor(23, 16)
+motor3_enable = OutputDevice(12, initial_value=1)
+
 GPIO = webiopi.GPIO
 
-LIGHT = 17 # GPIO pin using BCM numbering
+ENABLE_M2 = 17 # GPIO pin using BCM numbering
+ENABLE_M3 = 12
 
-HOUR_ON  = 8  # Turn Light ON at 08:00
-HOUR_OFF = 18 # Turn Light OFF at 18:00
+FORWARD_M2 = 6
+FORWARD_M3 = 23
+
+BACKWARD_M2 = 22
+BACKWARD_M3 = 16
+
+#HOUR_ON  = 8  # Turn Light ON at 08:00
+#HOUR_OFF = 18 # Turn Light OFF at 18:00
 
 # setup function is automatically called at WebIOPi startup
 def setup():
     # set the GPIO used by the light to output
-    GPIO.setFunction(LIGHT, GPIO.OUT)
+    GPIO.setFunction(ENABLE_M2, GPIO.OUT)
+    GPIO.setFunction(ENABLE_M3, GPIO.OUT)
 
-    # retrieve current datetime
-    now = datetime.datetime.now()
+    # enable motor
+    GPIO.digitalWrite(ENABLE_M2, GPIO.HIGH)
+    GPIO.digitalWrite(ENABLE_M3, GPIO.HIGH)
 
-    # test if we are between ON time and tun the light ON
-    if ((now.hour >= HOUR_ON) and (now.hour < HOUR_OFF)):
-        GPIO.digitalWrite(LIGHT, GPIO.HIGH)
+    # set GPIO motor control
+    GPIO.setFunction(FORWARD_M2,  GPIO.OUT)
+    GPIO.setFunction(BACKWARD_M2, GPIO.OUT)
+
+    GPIO.setFunction(FORWARD_M3,  GPIO.OUT)
+    GPIO.setFunction(BACKWARD_M3, GPIO.OUT)
 
 # loop function is repeatedly called by WebIOPi 
 def loop():
