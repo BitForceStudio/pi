b62b696b0c926d838d546a2e6134fc42ba473694
Bitforce.studio <BitForce.Studio@gmail.com>
2016-07-08 22:03:14 +0000

    solved the button down and send commend problem

--- vrcar/carcontrol/server/carcontrol/realtimecontrol/static/js/control.js
+++ vrcar/carcontrol/server/carcontrol/realtimecontrol/static/js/control.js
@@ -4,7 +4,8 @@ $(function() {
     //var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + window.location.pathname);
 
     var $commendbutton = $("#control").find('button[type="button"]');
-    
+    var mouseStillDown = false;
+
     //chatsock.onmessage = function(message) {
     //    var data = JSON.parse(message.data);
     //    var msg = $("#showmessage")
@@ -12,16 +13,40 @@ $(function() {
     //    msg.append(data)
     //};
 
-    $commendbutton.on("click",function(event){
+    $commendbutton.mousedown(function(event){
         var commend=this.id;
+        mouseStillDown=true;
+        mouseDownCommend(commend)
         //chatsock.send(str(commend));
 
         //document.getElementById("showmessage").value+="-up";
         return false;
     });
 
-    //document.body.onmousedown = function() { 
-    //    document.getElementById("showmessage").value+="+up";
-    //}
+    $commendbutton.mouseup(function(event){
+        //var commend=this.id;
+        mouseStillDown=false;
+        //mouseDownCommend(commend)
+        //chatsock.send(str(commend));
+
+        //document.getElementById("showmessage").value+="-up";
+        //return false;
+    });
+
+    function sleep (time) {
+        return new Promise((resolve) => setTimeout(resolve, time));
+    }
+
+    function mouseDownCommend(commend)
+    { 
+        document.getElementById("showmessage").value+=(commend);
+
+        if (mouseStillDown)
+        {
+            setTimeout(function(){
+                mouseDownCommend(commend)
+            }, 100);
+        }
+    }
 
 });
