e0fbd623db7a1597cc22dfa239abf290a33ad442
Bitforce.studio <BitForce.Studio@gmail.com>
2016-07-30 11:12:33 +0000

    full screen

--- vrcar/carcontrol/server/carcontrol/chatroom/static/control.js
+++ vrcar/carcontrol/server/carcontrol/chatroom/static/control.js
@@ -145,6 +145,26 @@ $(function() {
 
 });
 
+function toggle_fullscreen(e) {
+
+    var background = document.getElementById("background");
+
+    if(!background) {
+        background = document.createElement("div");
+        background.id = "background";
+        document.body.appendChild(background);
+    }
+  
+    if(e.className == "fullscreen") {
+        e.className = "";
+        background.style.display = "none";
+    }
+    else {
+        e.className = "fullscreen";
+        background.style.display = "block";
+    }
+
+}
 
 function reload_img () {
     if(!halted) 
