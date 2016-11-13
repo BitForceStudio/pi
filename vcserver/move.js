8d5cf3258d038343c6a85bf07cddb07715ddba22
bitforce.studio <bitforce.studio@gmail.com>
2016-11-13 19:26:02 +0000

    update the code

--- vcserver/move.js
+++ vcserver/move.js
@@ -24,6 +24,9 @@ var DEBUG = true;
 var RETRIES = 10;  // max number of retries for I2C calls
 //---------------------------------------------
 
+var pan  = 0;
+var tilt = 1;
+var step = 5;
 
 //---------------------------------------------
 // Initialise the Board (same as cleanup)
@@ -45,7 +48,74 @@ function init(debug=false){
 }
 //---------------------------------------------
 
-init();
+try{
+  initall();
+}
+catch(e){
+  console.log("define 2 i2c error"+e);
+}
+
+var panVal = 90;
+var tiltVal = 90;
+
+function initall(){
+  // init
+  init(true);
+  setOutputConfig(pan, 2);
+  setOutputConfig(tilt, 2);
+  setOutput(pan,  90 );
+  setOutput(tilt, 90 );
+}
+
+//---------------------------------------------
+// Set configuration of selected output
+// 0: On/Off, 1: PWM, 2: Servo, 3: WS2812B
+function setOutputConfig (output, value){
+  if (output>=0 && output<=5 && value>=0 && value<=3){
+    if (DEBUG)
+      console.log("set output config called, cmd:"+output+" value:"+value);
+    for (var i=0;i<RETRIES;i++){
+      try{
+        wire.writeByteSync(address, OUTCFG0+output, value);
+        break;
+      }
+      catch(e){
+        if (DEBUG)
+          console.log("Error in setOutputConfig(), retrying");
+      }
+    }
+  }
+}
+//---------------------------------------------
+
+//---------------------------------------------
+// Set output data for selected output channel
+// Mode  Name    Type    Values
+// 0     On/Off  Byte    0 is OFF, 1 is ON
+// 1     PWM     Byte    0 to 100 percentage of ON time
+// 2     Servo   Byte    -100 to + 100 Position in degrees
+// 3     WS2812B 4 Bytes 0:Pixel ID, 1:Red, 2:Green, 3:Blue
+function setOutput (channel, value){
+  if (channel>=0 && channel<=5){
+    if (DEBUG)
+      console.log("set output called");
+    for (var i=0;i<RETRIES;i++){
+      try{
+        wire.writeByteSync(address, OUTPUT0 + channel, value);
+        if(DEBUG)
+          console.log('set output, channel: '+channel+" value:"+value);
+        break;
+      }
+      catch(e){
+        if (DEBUG)
+          console.log("Error in setOutput(), retrying"+e+" "+value);
+      }
+    }
+    if (i==RETRIES){
+      cleanup();
+    }
+  }
+}
 
 //---------------------------------------------
 // Cleanup the Board (same as init)
@@ -114,7 +184,7 @@ app.get('/', function(req, res){
 app.use(express.static(path.join(__dirname, '')));
 
 io.on('connection', function(socket){
-  socket.on('control', function(msg){
+  socket.on('move', function(msg){
     var data = JSON.parse(msg);
     var x = data.x;
     var y = data.y;
@@ -147,6 +217,20 @@ io.on('connection', function(socket){
       forward(leftspeed,rightspeed);
     }
     console.log('message: x  speed: ' + xx+' leftspeed '+leftspeed+' y  speed:'+yy+' rightspeed '+rightspeed);
+    sleep.usleep(10);    
+  });
+  
+  socket.on('control', function(msg){
+    var data = JSON.parse(msg);
+    var x = data.x;
+    var y = 180-data.y;
+    console.log('message: x: ' + x+' y:'+y);
+    var xx = Math.min(140, x);
+    xx = Math.max(40,  xx);
+    setOutput (tilt, xx);
+    var yy = Math.max(40,  y);
+    yy = Math.min(150, yy);
+    setOutput (pan, yy);
     sleep.usleep(10);    
   });
 });
